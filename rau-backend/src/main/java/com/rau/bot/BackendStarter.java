2cd2c20a207a1c6e49f6bdbd7c612af28f42d792
John Vahanyan <jon.vahanyan@iunetworks.am>
2018-05-21 19:59:06 +0400

    Added backend starter

---
+++ rau-backend/src/main/java/com/rau/bot/BackendStarter.java
@@ -0,0 +1,11 @@
+package com.rau.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(BackendStarter.class, args);
+    }
+}
